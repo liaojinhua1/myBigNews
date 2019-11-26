f6277f5f337083aba2b54c6395d5a6d89e4cc48a
liaojinhua1 <Jin9Code@163.com>
2019-11-27 00:34:15 +0800

    index页面和article页面渲染完成

--- js/http.js
+++ js/http.js
@@ -9,7 +9,8 @@
         latest: baseURL + '/index/latest',//最新资讯
         latest_comment: baseURL + '/index/latest_comment',//最新评论
         attention: baseURL + '/index/attention',//焦点关注
-        artitle: baseURL + '/index/artitle',//文章详细内容
+        // http://localhost:8080/api/v1/index/article
+        article: baseURL + '/index/article',//文章详细内容
         post_comment: baseURL + '/index/post_comment',//发表评论
         get_comment: baseURL + '/index/get_comment',//评论列表
 
